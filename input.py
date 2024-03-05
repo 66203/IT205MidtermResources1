44302202c0e1620a40855735b078ebd7d59ea312
Pawii <153156733+66203@users.noreply.github.com>
2024-03-05 15:35:14 +0800

    Add files via upload

---
+++ input.py
@@ -0,0 +1,10 @@
+#input() has string data type
+num1 = int (input("Enter 1st number: "))
+int (num1)
+#int( for converting striong to integer)
+num2 = int (input("Enter 2nd number: "))
+int(num2)
+
+sum = num1 + num2
+
+print(f"The sum of {num1} and {num2} is: {sum}")
